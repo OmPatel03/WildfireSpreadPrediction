410c975ef0ce4ea8adcf73294cde93f9ccdc3fb1
Om Patel <ompatel712003@gmail.com>
2025-11-17 16:11:26 -0500

    find_coord some work

---
+++ backend/utils/convert.py
@@ -0,0 +1,6 @@
+def find_coord(n, m , center_lat, center_long, row, col):
+    lat_step = 1
+    long_step = 1
+    zero_lat, zero_long = center_lat - (n // 2) * lat_step - lat_step / 2 * (n % 2), center_long - (m // 2) * long_step - long_step / 2 * (m % 2)
+    lat = zero_lat + row * lat_step
+    long = zero_long + col * long_step
